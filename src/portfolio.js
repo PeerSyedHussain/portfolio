88c27e03191ecd663c68aeef3226828c80525f71
peersyedhussain <syedhussain7558@gmail.com>
2022-10-21 01:13:31 +0530

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,453 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Syed Hussain",
+  title: "Hi all, I'm Syed",
+  subTitle: emoji(
+    "A passionate Frontend Developer / Designer 🚀 having an experience of building Web applications with JavaScript / Reactjs and some other cool libraries and frameworks. As Cybersecurity researcher 🚀 got acknowledged by million dollar companies"
+  ),
+  resumeLink: "", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/PeerSyedHussain",
+  linkedin: "https://www.linkedin.com/in/peer-syed-hussain-s-91a625181/",
+  gmail: "devsyed04@gmail.com",
+  gitlab: "",
+  facebook: "",
+  medium: "https://medium.com/@syedhussy",
+  stackoverflow: "",
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle:
+    "AS CRAZY DEVELOPER / DESIGNER / SECURITY RESEARCHER BELOW ARE THE EXPLORED TECH STACK WHICH I USED",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) using Frameworks & Library"),
+    emoji("⚡ Integration of third party services"),
+    emoji(
+      "⚡ As cybersecurity researcher protect organisation from malicious action"
+    ),
+    emoji("⚡ Acknowledged by million dollar companies")
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "bootstrap4",
+      fontAwesomeClassname: "fab fa-bootstrap"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "javaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "php",
+      fontAwesomeClassname: "fab fa-php"
+    },
+    {
+      skillName: "wordpress",
+      fontAwesomeClassname: "fab fa-wordpress"
+    },
+    {
+      skillName: "figma",
+      fontAwesomeClassname: "fab fa-figma"
+    },
+    {
+      skillName: "invision",
+      fontAwesomeClassname: "fab fa-invision"
+    },
+    {
+      skillName: "github",
+      fontAwesomeClassname: "fab fa-github"
+    },
+    {
+      skillName: "gitlab",
+      fontAwesomeClassname: "fab fa-gitlab"
+    },
+    {
+      skillName: "linux",
+      fontAwesomeClassname: "fab fa-linux"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "security-analyst",
+      fontAwesomeClassname: "fas fa-user-secret"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Dr.SACOE",
+      logo: require("./assets/images/dr-sacoe.png"
+        ? "./assets/images/dr-sacoe.png"
+        : ""),
+      subHeader: "Bachelor of Engineering in ECE",
+      duration: "August 2015 - April 2019",
+      desc: "Distinction",
+      descBullets: ["", ""]
+    },
+    {
+      schoolName: "Angelo MHSS",
+      logo: require("./assets/images/school-placeholder.png"
+        ? "./assets/images/school-placeholder.png"
+        : ""),
+      subHeader: "Class 12",
+      duration: "March 2014 - March 2015",
+      desc: "Ranked top 3%",
+      descBullets: [
+        "Received Award From School Education Department",
+        "Graduated in Top 84% of Class in HSC"
+      ]
+    },
+    {
+      schoolName: "Angelo MHSS",
+      logo: require("./assets/images/school-placeholder.png"
+        ? "./assets/images/school-placeholder.png"
+        : ""),
+      subHeader: "Class 10",
+      duration: "March 2012 - March 2013",
+      desc: "Ranked top 1%",
+      descBullets: [
+        "Received Awards from Tamgrads & SchoolEducation Department",
+        "Received Award From Friends Charitable Trust",
+        "Graduated in Top 96.4% of Class in SSLC"
+      ]
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Pentesting",
+      progressPercentage: "70%"
+    },
+    {
+      Stack: "Logical Thinking",
+      progressPercentage: "80%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "UI Intern",
+      company: "Tiliconveli",
+      companylogo: require("./assets/images/tiliconveli.png"),
+      date: "April 2019 – Jun 2019",
+      desc: "Joined as intern",
+      descBullets: [
+        "Created site layout and user interface using HTMLand CSS practices",
+        "Took concepts and produced design mock-ups and prototypes to strengthen designs, enhance user experiences and improve site."
+      ]
+    },
+    {
+      role: "UI Developer",
+      company: "Smackcoders",
+      companylogo: require("./assets/images/smackcoders.png"),
+      date: "July 2019 – Dec 2019",
+      desc: "Joined as junior ui developer",
+      descBullets: [
+        "Used various technologies, including Software tomake code repairs and optimize corporate website",
+        "Designed, implemented and monitored web pagesand sites for continuous improvement",
+        "Created site layout and user interface using HTMLand CSS practices"
+      ]
+    },
+    {
+      role: "Fullstack Developer",
+      company: "JSM Private Limited",
+      companylogo: require("./assets/images/jsm.png"),
+      companyLogoClassName: "scale-down",
+      date: "Jan 2020 – Oct 2020",
+      desc: "Joined as full stack developer",
+      descBullets: [
+        "Wrote code on Production & Development platform Using Frameworks",
+        "Maintained existing applications and designed and delivered new applications",
+        "Discussed project progress with customers, collected feedback on different stages and directly addressed concerns."
+      ]
+    },
+    {
+      role: "Frontend Developer & Cybersecurity Researcher",
+      company: "Doodleblue",
+      companylogo: require("./assets/images/doodleblue.png"),
+      companyLogoClassName: "scale-down",
+      date: "Nov 2020 – Present",
+      desc: "Joined as frontend developer & extend role by security analyst",
+      descBullets: [
+        "Collaborated with stakeholders during development processes to confirm creative proposals and design best practices.",
+        "Identified issues, analyzed information and provided solutions to problems",
+        "Used coordination and planning skills to achieve results according to schedule"
+      ]
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "false", // Set true or false to show Contact profile using Github, defaults to true
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      projectName: "Saayahealth",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "http://saayahealth.com/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/nextuLogo.webp"),
+      projectName: "Nextu",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "http://nextu.se/"
+        }
+      ]
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle:
+        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      image: require("./assets/images/codeInLogo.webp"),
+      imageAlt: "Google Code-In Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+        },
+        {
+          name: "Award Letter",
+          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
+        },
+        {
+          name: "Google Code-in Blog",
+          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+        }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle:
+        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      imageAlt: "Google Assistant Action Logo",
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+        }
+      ]
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      imageAlt: "PWA Logo",
+      footerLink: [
+        {name: "Certification", url: ""},
+        {
+          name: "Final Project",
+          url: "https://pakistan-olx-1.firebaseapp.com/"
+        }
+      ]
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/"
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  // number: "+92-0000000000",
+  email_address: "devsyed04@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "freakscrs_faizy", //Replace "twitter" with your twitter username without @
+  display: false // Set true to display this section, defaults to false
+};
+
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+  isHireable
+};
